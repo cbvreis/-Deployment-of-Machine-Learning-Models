a21a23eb8180900cc7fa032c80a38c8e0c842dd4
Cássio Reis <cassio.reis@engenharia.ufjf.br>
2022-07-04 09:37:14 -0300

    Data Analysis

---
+++ main.py
@@ -0,0 +1,30 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import smtpd
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    import pandas as pd
+    df = pd.DataFrame(
+        {
+            'columns':df_dados.columns,
+            'nulls':df_dados.isnull().sum(),
+            'dtypes':df_dados.dtypes,
+
+        }
+    )
+
+#add two numbers
+def add(a,b):
+    return a+b
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
